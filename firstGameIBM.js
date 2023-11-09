1c199d06c5bc891044ec9a962017de8480aef765
vinicius <vinicius.belucci@outlook.com>
2023-11-09 15:56:18 -0300

    mais exercicios

--- firstGameIBM.js
+++ firstGameIBM.js
@@ -12,20 +12,43 @@ function position(number) {
   const [gamer, numberScores, ...rest] = number;
 
   let position = 1;
-
   for (let j = 1; j <= numberScores; j++) {
     let maior = rest.reduce((acc, curr) => Math.max(acc, curr), 0);
 
     j = position;
     for (let i = 0; i < rest.length; i++) {
       const index = rest.indexOf(maior, i);
+
+      if (maior == 0) {
+        return rest;
+      }
+
       if (index != -1) {
-        rest[index] = j;
+        rest[index] = `${j}`;
         position++;
       }
     }
   }
-  return rest;
+  const quantity = rest.filter((x) => {
+    const isNumber = typeof x;
+    if (isNumber == "string") {
+      return x;
+    }
+  });
+
+  const array = rest.map((n) => {
+    const isNumber = typeof n;
+    if (isNumber == "number") {
+      return (n = "desclassificado");
+    }
+    return n;
+  });
+
+  return {
+    totalJogadores: rest.length,
+    jogadoresProximaFase: quantity.length,
+    array: array,
+  };
 }
 
 console.log(position(number));
---
+++ secondGameIBM.js
@@ -0,0 +1,29 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+const levelUp = (lines) => {
+  const [numberGames, cutoff, ...rest] = lines;
+  const number = rest.map((n) => +n);
+
+  let select = [];
+
+  for (let i = 0; i < cutoff; i++) {
+    let maior = number.reduce((acc, curr) => Math.max(acc, curr), 0);
+
+    const index = number.indexOf(maior);
+
+    if (maior == 0) {
+      return select;
+    }
+
+    if (index !== -1) {
+      const maiorNumero = number.splice(index, 1);
+
+      select.push(maiorNumero);
+    }
+  }
+
+  return select;
+};
+
+console.log(levelUp(lines));
